85a38ea3f11f111f0b84fe1af6be1f2c4276377b
LOGANSPC\Benjamin <nnpdrummer@gmail.com>
2017-02-03 18:52:39 -0500

    PL04 Starting basic dialogue system.

---
+++ Assets/Script/DialogueNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class DialogueNode
+{
+    private int nodeID;
+    private string characterName;
+    private string prompt;
+    private string[] responses;
+
+    public DialogueNode()
+    {
+        nodeID = -2;
+        characterName = "";
+        prompt = "";
+        responses = new string[0];
+    }
+
+    public DialogueNode(int newNodeID, string newName, string newPrompt, string[] newResponses)
+    {
+        nodeID = newNodeID;
+        characterName = newName;
+        prompt = newPrompt;
+        copyArray(newResponses);
+    }
+
+    private void copyArray(string[] newResponses)
+    {
+        responses = new string[newResponses.Length];
+
+        for(int responseIterator = 0; responseIterator < newResponses.Length; responseIterator++)
+        {
+            responses[responseIterator] = newResponses[responseIterator];
+        }
+    }
+}
---
+++ Assets/Script/DialogueSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour {
+
+    public TextAsset textFile;
+    private string[] textLines;
+    private char[] p;
+    public int nodeCounter;
+    public string element1;
+
+    private void Start()
+    {
+        if (textFile != null)
+        {
+            textLines = textFile.text.Split('\n');
+        }
+        CopyLines();
+        CreateNodes();
+    }
+
+    private void CopyLines()
+    {
+        p = new char[textLines[0].Length];
+        for (int pI = 0; pI < textLines[0].Length; pI++)
+        {
+            p[pI] = textLines[0][pI];
+            Debug.Log(p[pI]);
+        }
+
+        Debug.Log(p[1] == '\n');
+    }
+
+    private void CreateNodes()
+    {
+        nodeCounter = 0;
+
+        //element1 = p[2];
+
+        // Debug.Log(p[2]);
+        //Debug.Log("updated" + p[2].Equals("Ugghhhh, today is such a long day; can't wait to go home and smell my dog."));
+
+        //for (int lineIterator = 0; lineIterator < textLines.Length; lineIterator++)
+        //{
+        //    if (textLines[lineIterator].Equals("0 "))
+        //    {
+        //        Debug.Log("Did we get here?");
+        //        nodeCounter++;
+        //    }
+        //}
+    }
+}
